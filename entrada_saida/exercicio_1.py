8266c3f89f0157e4d7ef446254fd1188c5441a38
Rafael0703 <rafael.c.albino@gmail.com>
2024-05-07 09:40:26 -0300

    Create exercicio_1.py

---
+++ entrada_saida/exercicio_1.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Insira o primeiro número inteiro: "))
+numero2 = int(input("Insira o segundo número inteiro: "))
+print("Você inseriu os números:", numero1, "e", numero2)
+soma = (numero1 + numero2)
+produto = (numero1 + numero2)
+print(f"A soma dos números:{ soma }")
