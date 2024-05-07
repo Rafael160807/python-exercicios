431e7ae3c70fecbb65f877472031be34288e673e
Rafael0703 <rafael.c.albino@gmail.com>
2024-05-07 11:08:52 -0300

    Create exercicio_4.py

---
+++ entrada_saida/exercicio_4.py
@@ -0,0 +1,5 @@
+nota1 = float(input("Insira a 1º nota:"))
+nota2 = float(input("Insira a 2º nota:"))
+nota3 = float(input("Insira a 3º nota:"))
+media = (nota1+nota2+nota3)/3
+print(f"A sua media final é: {media:.02f} ")
