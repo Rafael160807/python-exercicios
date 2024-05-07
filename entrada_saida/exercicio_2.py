e5d9ae2dbdc63ea4a964e76c6a3deeb24ad93805
Rafael0703 <rafael.c.albino@gmail.com>
2024-05-07 10:29:05 -0300

    Create exercicio_2.py

---
+++ entrada_saida/exercicio_2.py
@@ -0,0 +1,4 @@
+estatura = float(input("Insira sua estatura em metros : "))
+print("O numero da sua estatura é :", estatura ,"metros")
+conversao = estatura * 100
+print(f"A conversão da sua estatura em metros para centimetros é: { conversao } centimetros") 
